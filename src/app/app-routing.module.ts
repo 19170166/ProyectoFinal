e671eff215aba44cbcfd8d1eb36b5f304e07391e
19170166 <71044488+19170166@users.noreply.github.com>
2021-03-16 18:45:20 -0600

    Primer Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetallesComponent } from './Componentes/detalles/detalles.component';
 
-const routes: Routes = [];
+import { PaginaPrincipalComponent } from './Componentes/pagina-principal/pagina-principal.component';
+
+
+const routes: Routes = [
+  {path: 'Inicio', component:PaginaPrincipalComponent},
+  {path: 'Detalle', component:DetallesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './Componentes/nav/nav.component';
+import { PaginaPrincipalComponent } from './Componentes/pagina-principal/pagina-principal.component';
+import { DetallesComponent } from './Componentes/detalles/detalles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    PaginaPrincipalComponent,
+    DetallesComponent
   ],
   imports: [
     BrowserModule,
