f6199ee2c90355ef97d288baa4b3e1fa4ed4b1e5
19170166 <71044488+19170166@users.noreply.github.com>
2021-03-17 10:34:44 -0600

    Primer Commit

--- src/app/Componentes/detalles/detalles.component.ts
+++ src/app/Componentes/detalles/detalles.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetallesComponent implements OnInit {
 
-  n:number = 10
+  n:number = 9
   
   constructor() { }
   ngOnInit(): void {
